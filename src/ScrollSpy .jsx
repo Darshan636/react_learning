a4592bba2eb2204c2042421bac359b646e78504b
MohammadSadiqmaniyar <mohammadsadiqmaniyar786@gmail.com>
2025-03-26 09:31:07 +0530

    day01

---
+++ src/ScrollSpy .jsx
@@ -0,0 +1,41 @@
+function ScrollSpy(){
+    return(
+        <>
+        <br></br>
+        <br></br>
+        <div class="row">
+  <div class="col-4">
+    <div id="simple-list-example" class="d-flex flex-column gap-2 simple-list-example-scrollspy text-center">
+      <a class="p-1 rounded" href="#simple-list-item-1">About ME</a>
+      <a class="p-1 rounded" href="#simple-list-item-2">Qualification</a>
+      <a class="p-1 rounded" href="#simple-list-item-3">Contact Info</a>
+      {/* <a class="p-1 rounded" href="#simple-list-item-4">Item 4</a> */}
+      {/* <a class="p-1 rounded" href="#simple-list-item-5">Item 5</a> */}
+    </div>
+  </div>
+  <div class="col-8">
+    <div data-bs-spy="scroll" data-bs-target="#simple-list-example" data-bs-offset="0" data-bs-smooth-scroll="true" class="scrollspy-example" tabindex="0">
+      <h4 id="simple-list-item-1">About Me</h4>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos velit ea quia sunt soluta illum. Nulla, ipsam illum. Explicabo repudiandae minima corporis itaque enim qui!</p>
+      <h4 id="simple-list-item-2">Qualification</h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi blanditiis soluta id laboriosam, quasi reprehenderit similique ut culpa dolor praesentium, reiciendis quibusdam cumque rem beatae?</p>
+      <h4 id="simple-list-item-3">Contact Info</h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae in vel iure ut praesentium quo quis nulla, veniam possimus reprehenderit, facere nihil. Non eius at nisi adipisci aliquid optio ut!</p>
+      {/* <h4 id="simple-list-item-4">Item 4</h4>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi omnis dolor deserunt quibusdam dolores quae similique aspernatur rerum minima autem, eum numquam? Earum, sit atque.</p>
+      <h4 id="simple-list-item-5">Item 5</h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos voluptatibus delectus rem hic, possimus adipisci voluptatem accusantium. Earum, eligendi rerum nulla eaque hic quas alias?</p> */}
+    </div>
+  </div>
+</div>
+<br></br>
+<br></br>
+<br></br>
+<br></br>
+
+
+        </>
+    )
+}
+
+export default ScrollSpy;
